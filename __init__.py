f2065ee7ca0bcf35ea8f6553a50e05facb7debbc
oliverbinns <oliverbinns@gmail.com>
2016-05-18 16:29:44 +0200

    Added init for python module

---
+++ __init__.py
@@ -0,0 +1,21 @@
+"""
+timeWarpOB is a python Dynamic Time Warping implementation
+Version 0.1
+
+For more information, see the main documentation in /docs/
+Oliver Binns
+"""
+
+# Define python imports
+from __future__ import division, print_function
+
+# Define timeWarp imports
+from .timeWarpOB import *
+import tests
+
+# Define colours for display
+_col0 = '\033[0m'
+_col1 = '\033[95m'
+_col2 = '\033[92m'
+
+# Any startup code here
