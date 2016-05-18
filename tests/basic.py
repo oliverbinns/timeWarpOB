e309a97158e03ec6a9c8d9867bcfc35bc9db55c5
oliverbinns <oliverbinns@gmail.com>
2016-05-18 18:42:52 +0200

    Added basic sin / cos testing routines

--- tests/basic.py
+++ tests/basic.py
@@ -1,1 +1,24 @@
 # Tests (to be added)
+
+def sinCos(n=2):
+	''' Generates a test dataset of sin and cosine over 200
+	points in a time series
+	'''
+	import numpy as np
+
+	ts = list(np.linspace(0, n*np.pi, 200))
+	x = list(np.sin(ts))
+	y = list(np.cos(ts))
+
+	return ts, x, y
+
+def testWarp(x,y,ts):
+	''' Warps two time series, displays plots and prints 
+		the warp statistics
+	'''
+	import timeWarpOB as tw
+
+	wo = tw.timeWarp(x,y)
+	tw.plotting.plotWarp(x,y,wo)
+
+	print wo['warpStats']
