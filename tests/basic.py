cc18d53474727781d7deecbaccf3d266aa05cbe4
oliverbinns <oliverbinns@gmail.com>
2016-05-19 16:29:23 +0200

    Update of tests (bug fix on print)

--- tests/basic.py
+++ tests/basic.py
@@ -1,8 +1,20 @@
-# Tests (to be added)
-
 def sinCos(n=2):
 	''' Generates a test dataset of sin and cosine over 200
-	points in a time series
+	points in a time series.
+
+	Parameters
+	----------
+		n : int
+			Number of half-cycles to calculate the values over
+
+	Returns
+	-------
+		x : list
+			A list of sin(ts) values
+		y : list
+			A list of cos(ts) values
+		ts : list
+			A list of 'timestamp' values (radian angle values)
 	'''
 	import numpy as np
 
@@ -10,15 +22,32 @@ def sinCos(n=2):
 	x = list(np.sin(ts))
 	y = list(np.cos(ts))
 
-	return ts, x, y
+	return x,y,ts
 
 def testWarp(x,y,ts):
 	''' Warps two time series, displays plots and prints 
-		the warp statistics
+		the warp statistics.
+
+	Parameters
+	----------
+		x : list
+			First time series, which has been warped against time series b
+		y : list 
+			Second time series (reference)
+		ts : list
+			Optional list of timestamps for displaying on the graphs. If no timestamps are 
+			given, each time period will be given an incremented number, staring at 1.
+
+	Returns
+	-------
+		matplotlib.pyplot
+			A composite plot showing the results of the time warp.
+		warpStats : dict
+			Warp statistics (printed to the console only)
 	'''
 	import timeWarpOB as tw
 
 	wo = tw.timeWarp(x,y)
 	tw.plotting.plotWarp(x,y,wo)
 
-	print wo['warpStats']
+	print(wo['warpStats'])
