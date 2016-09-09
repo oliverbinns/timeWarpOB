591e4547c7939eb35263dc64d3e19385a0e5255f
oliverbinns <oliverbinns@gmail.com>
2016-09-09 10:07:24 +0200

    Push of version 1.1.1 to pyPI

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='timeWarpOB',
-    version='1.1.0',
+    version='1.1.1',
     description='A Dynamic Time Warping implementation',
     long_description=long_description,
     url='https://github.com/oliverbinns/timeWarpOB',
